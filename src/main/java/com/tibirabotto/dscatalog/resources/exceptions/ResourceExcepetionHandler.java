281ca08de24215053bff386e5ee03f02a3854f25
Tibira <tibira.botto@gmail.com>
2023-10-08 18:52:29 -0400

    Update category

--- src/main/java/com/tibirabotto/dscatalog/resources/exceptions/ResourceExcepetionHandler.java
+++ src/main/java/com/tibirabotto/dscatalog/resources/exceptions/ResourceExcepetionHandler.java
@@ -7,15 +7,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.tibirabotto.dscatalog.services.exceptions.EntityNotFoundException;
+import com.tibirabotto.dscatalog.services.exceptions.ResourcesNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ResourceExcepetionHandler {
 	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+	@ExceptionHandler(ResourcesNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourcesNotFoundException e, HttpServletRequest request) {
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(HttpStatus.NOT_FOUND.value());
--- src/main/java/com/tibirabotto/dscatalog/services/CategoryService.java
+++ src/main/java/com/tibirabotto/dscatalog/services/CategoryService.java
@@ -1,6 +1,5 @@
 package com.tibirabotto.dscatalog.services;
 
-
 import java.util.List;
 import java.util.Optional;
 
@@ -14,36 +13,49 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.tibirabotto.dscatalog.dto.CategoryDTO;
 import com.tibirabotto.dscatalog.entities.Category;
 import com.tibirabotto.dscatalog.repositories.CategoryRepository;
-import com.tibirabotto.dscatalog.services.exceptions.EntityNotFoundException;
+import com.tibirabotto.dscatalog.services.exceptions.ResourcesNotFoundException;
 
-
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-		
+
 	}
-	
+
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(@PathVariable Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
-		
+		Category entity = obj.orElseThrow(() -> new ResourcesNotFoundException("Entity not found"));
+
 		return new CategoryDTO(entity);
 	}
-	
+
 	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
 		Category entity = new Category();
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourcesNotFoundException("ID not found" + id);
+		}
+
 	}
 }
