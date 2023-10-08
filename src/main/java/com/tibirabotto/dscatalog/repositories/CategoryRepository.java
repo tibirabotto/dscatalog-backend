ccb6948a00d9b75327cfea95b658e6f073947681
Tibira <tibira.botto@gmail.com>
2023-10-07 23:31:30 -0400

    H2 database

---
+++ src/main/java/com/tibirabotto/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.tibirabotto.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tibirabotto.dscatalog.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
